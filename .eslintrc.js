a084099b90d65a0663adec37804426e72c65edaa
azu <azuciao@gmail.com>
2022-07-10 12:51:44 +0900

    add jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,13 +25,6 @@ module.exports = {
     },
     overrides: [
         {
-            files: ["apps/**/*"],
-            rules: {
-                // App pages require default exports
-                "import/no-default-export": "off"
-            }
-        },
-        {
             files: ["*.config.js", ".eslintrc.js"],
             rules: {
                 "sort-keys": "off",
---
+++ babel.config.js
@@ -0,0 +1,5 @@
+// https://www.npmjs.com/package/babel-preset-beemo
+module.exports = {
+    // This is mainly used by Jest
+    presets: ["babel-preset-beemo"]
+};
